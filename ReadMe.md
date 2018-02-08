596271eb56f998e74cf9ba72d4cffe6f1b508b26
Yakim Rachev <jakimcho@gmail.com>
2018-02-08 18:31:08 +0200

    [Following APIv4 Setup Guide](https://developers.google.com/analytics/devguides/reporting/core/v4/quickstart/service-java)

---
+++ ReadMe.md
@@ -0,0 +1,64 @@
+# Flow of setup engine that collects data from google analytics
+
+## API
+
+The API version that is going to be use will be [Google Analytics Reporting API v4 API](https://developers.google.com/analytics/devguides/reporting/core/v4/).
+
+> The Google Analytics Reporting API v4 is the most advanced programmatic method to access report data in Google Analytics. With the Google Analytics Reporting API, you can:
+>
+> Build custom dashboards to display Google Analytics data.
+> Automate complex reporting tasks to save time.
+> Integrate your Google Analytics data with other business applications.```
+
+### Google Analytics Reporting APIv4 Features:
+
+> Google Analytics is built upon a powerful data reporting infrastructure. 
+> The Google Analytics Reporting API v4 gives you access to the power of the Google Analytics platform. 
+> The API provides these key features:
+>
+> - Metric expressions
+>
+> The API allows you to request not only built-in metrics but also combination of metrics expressed in mathematical operations. 
+> For example, you can use the expression `ga:goal1completions/ga:sessions` to request the goal completions per number of sessions.
+>
+> - Multiple date ranges
+>
+> The API allows you in a single request to get data in two date ranges.
+>
+> - Cohorts and Lifetime value
+>
+> The API has a rich vocabulary to request Cohort and Lifetime value reports.
+> 
+> - Multiple segments
+> 
+> The API enables you to get multiple segments in a single request. 
+
+## Authorization Flows
+
+There are are several ways to authorize your app in order to access Google account data.
+[Read here for details](https://developers.google.com/analytics/devguides/reporting/core/v4/authorization)
+
+The one that best fits our need is:
+
+[**Service Accounts**](https://developers.google.com/analytics/devguides/reporting/core/v4/quickstart/service-java)
+> To get started using Analytics Reporting API v4, you need to first [use the setup tool](https://console.developers.google.com/start/api?id=analyticsreporting.googleapis.com&credential=client_key), 
+> which guides you through creating a project in the Google API Console, 
+> enabling the API, and creating credentials.
+
+---------------------
+
+Core Reporting API and Management API
+
+Service accounts are useful for automated, offline, or scheduled access to Google Analytics data for your own account. 
+For example, to build a live dash-board of your own Google Analytics data and share it with other users.
+
+To get started using Analytics API, you need to first use the setup tool, which guides you through creating a project in the Google API Console, 
+enabling the API, and creating credentials.
+
+To set up a new service account, do the following:
+
+1. Click Create credentials > Service account key.
+2. Choose whether to download the service account's public/private key as a standard P12 file, or as a JSON file that can be loaded by a Google API client library.
+
+Your new public/private key pair is generated and downloaded to your machine; it serves as the only copy of this key. 
+You are responsible for storing it securely.
--- src/com/qualityhoue/automation/Main.java
+++ src/com/qualityhoue/automation/Main.java
@@ -1,6 +1,7 @@
 package com.qualityhoue.automation;
 
 import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -8,6 +9,7 @@ import java.security.GeneralSecurityException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 
 import com.google.api.client.auth.oauth2.Credential;
 import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
@@ -18,98 +20,181 @@ import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.http.HttpTransport;
 import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.gson.GsonFactory;
 import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.client.util.store.FileDataStoreFactory;
 import com.google.api.services.analyticsreporting.v4.AnalyticsReporting;
 import com.google.api.services.analyticsreporting.v4.AnalyticsReportingScopes;
+import com.google.api.services.analyticsreporting.v4.model.ColumnHeader;
 import com.google.api.services.analyticsreporting.v4.model.DateRange;
+import com.google.api.services.analyticsreporting.v4.model.DateRangeValues;
 import com.google.api.services.analyticsreporting.v4.model.Dimension;
 import com.google.api.services.analyticsreporting.v4.model.GetReportsRequest;
 import com.google.api.services.analyticsreporting.v4.model.GetReportsResponse;
 import com.google.api.services.analyticsreporting.v4.model.Metric;
+import com.google.api.services.analyticsreporting.v4.model.MetricHeaderEntry;
+import com.google.api.services.analyticsreporting.v4.model.Report;
 import com.google.api.services.analyticsreporting.v4.model.ReportRequest;
+import com.google.api.services.analyticsreporting.v4.model.ReportRow;
 
 public class Main {
 
-	private static HttpTransport httpTransport;
-	private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
-	private static final String APPLICATION_NAME = "DAIMTO-GoogleAnalyticsReportingSample/1.0";
-	private static FileDataStoreFactory dataStoreFactory;
-
-	private static String TOKEN = "ya29.GltaBeqGKdJD_T1UCauws3k90vYaqI7mkqx6zj-4FomWqJk_"
-			+ "CbS9JOnOkXBBL-vYsusUvxEFV9HmV9f5GQU9oiFKl65Ckezgua" + "FWHrxjDd9eRatfvnvLEeot9kd3";
+	private static final String APPLICATION_NAME = "Hello Analytics Reporting";
+	private static final JsonFactory JSON_FACTORY = GsonFactory.getDefaultInstance();
+	private static final String KEY_FILE_LOCATION = "<REPLACE_WITH_JSON_FILE>";
+	private static final String VIEW_ID = "<REPLACE_WITH_VIEW_ID>";
 
 	public static void main(String[] args) {
-		GoogleCredential credentaial = new GoogleCredential().setAccessToken(TOKEN);
-		DateRange dateRange = new DateRange();
-		dateRange.setStartDate("2018-02-06");
-		dateRange.setEndDate("2018-02-07");
+		try {
+			AnalyticsReporting service = initializeAnalyticsReporting();
 
+			GetReportsResponse response = getReport(service);
+			printResponse(response);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	//
+	// GoogleCredential credentaial = new
+	// GoogleCredential().setAccessToken(TOKEN);
+	// DateRange dateRange = new
+	// DateRange();dateRange.setStartDate("2018-02-06");dateRange.setEndDate("2018-02-07");
+	//
+	// Metric sessions = new
+	// Metric().setExpression("ga:sessions").setAlias("sessions");
+	//
+	// Dimension browser = new Dimension().setName("ga:browser");
+	//
+	// ReportRequest request = new
+	// ReportRequest().setViewId("ga:135032645").setDateRanges(Arrays.asList(dateRange))
+	// .setDimensions(Arrays.asList(browser)).setMetrics(Arrays.asList(sessions));
+	//
+	// ArrayList<ReportRequest> requests = new
+	// ArrayList<ReportRequest>();requests.add(request);
+	//
+	// GetReportsRequest getReport = new
+	// GetReportsRequest().setReportRequests(requests);
+	//
+	// try
+	// {
+	// httpTransport = GoogleNetHttpTransport.newTrustedTransport();
+	//
+	// Credential credential = authorize();
+	// AnalyticsReporting analyticsreporting = new
+	// AnalyticsReporting.Builder(httpTransport, JSON_FACTORY, credential)
+	// .setApplicationName(APPLICATION_NAME).build();
+	//
+	// GetReportsResponse response =
+	// analyticsreporting.reports().batchGet(getReport).execute();
+	// }catch(
+	// GeneralSecurityException e)
+	// {
+	// // TODO Auto-generated catch block
+	// e.printStackTrace();
+	// }catch(
+	// IOException e)
+	// {
+	// // TODO Auto-generated catch block
+	// e.printStackTrace();
+	// }catch(
+	// Exception e)
+	// {
+	// // TODO Auto-generated catch block
+	// e.printStackTrace();
+	// }
+	//
+	// }
+
+	/**
+	 * Initializes an Analytics Reporting API V4 service object.
+	 *
+	 * @return An authorized Analytics Reporting API V4 service object.
+	 * @throws IOException
+	 * @throws GeneralSecurityException
+	 */
+	private static AnalyticsReporting initializeAnalyticsReporting() throws GeneralSecurityException, IOException {
+
+		HttpTransport httpTransport = GoogleNetHttpTransport.newTrustedTransport();
+		GoogleCredential credential = GoogleCredential.fromStream(new FileInputStream(KEY_FILE_LOCATION))
+				.createScoped(AnalyticsReportingScopes.all());
+
+		// Construct the Analytics Reporting service object.
+		return new AnalyticsReporting.Builder(httpTransport, JSON_FACTORY, credential)
+				.setApplicationName(APPLICATION_NAME).build();
+	}
+
+	/**
+	 * Queries the Analytics Reporting API V4.
+	 *
+	 * @param service
+	 *            An authorized Analytics Reporting API V4 service object.
+	 * @return GetReportResponse The Analytics Reporting API V4 response.
+	 * @throws IOException
+	 */
+	private static GetReportsResponse getReport(AnalyticsReporting service) throws IOException {
+		// Create the DateRange object.
+		DateRange dateRange = new DateRange();
+		dateRange.setStartDate("7DaysAgo");
+		dateRange.setEndDate("today");
+
+		// Create the Metrics object.
 		Metric sessions = new Metric().setExpression("ga:sessions").setAlias("sessions");
 
-		Dimension browser = new Dimension().setName("ga:browser");
+		Dimension pageTitle = new Dimension().setName("ga:pageTitle");
 
-		ReportRequest request = new ReportRequest().setViewId("ga:135032645").setDateRanges(Arrays.asList(dateRange))
-				.setDimensions(Arrays.asList(browser)).setMetrics(Arrays.asList(sessions));
+		// Create the ReportRequest object.
+		ReportRequest request = new ReportRequest().setViewId(VIEW_ID).setDateRanges(Arrays.asList(dateRange))
+				.setMetrics(Arrays.asList(sessions)).setDimensions(Arrays.asList(pageTitle));
 
 		ArrayList<ReportRequest> requests = new ArrayList<ReportRequest>();
 		requests.add(request);
 
+		// Create the GetReportsRequest object.
 		GetReportsRequest getReport = new GetReportsRequest().setReportRequests(requests);
 
-		try {
-			httpTransport = GoogleNetHttpTransport.newTrustedTransport();
+		// Call the batchGet method.
+		GetReportsResponse response = service.reports().batchGet(getReport).execute();
 
-			Credential credential = authorize();
-			AnalyticsReporting analyticsreporting = new AnalyticsReporting.Builder(httpTransport, JSON_FACTORY,
-					credential).setApplicationName(APPLICATION_NAME).build();
-
-			GetReportsResponse response = analyticsreporting.reports().batchGet(getReport).execute();
-		} catch (GeneralSecurityException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
+		// Return the response.
+		return response;
 	}
 
-	private static Credential authorize() throws Exception {
-		// load client secrets
+	/**
+	 * Parses and prints the Analytics Reporting API V4 response.
+	 *
+	 * @param response
+	 *            An Analytics Reporting API V4 response.
+	 */
+	private static void printResponse(GetReportsResponse response) {
 
-		String workingDir = System.getProperty("user.dir");
-		System.out.println("Current working directory : " + workingDir);
-		String b;
-		BufferedReader io = new BufferedReader(new FileReader("./client_secrets.json"));
+		for (Report report : response.getReports()) {
+			ColumnHeader header = report.getColumnHeader();
+			List<String> dimensionHeaders = header.getDimensions();
+			List<MetricHeaderEntry> metricHeaders = header.getMetricHeader().getMetricHeaderEntries();
+			List<ReportRow> rows = report.getData().getRows();
 
-		try {
-			StringBuilder sb = new StringBuilder();
-			String line = io.readLine();
-
-			while (line != null) {
-				sb.append(line);
-				sb.append(System.lineSeparator());
-				line = io.readLine();
+			if (rows == null) {
+				System.out.println("No data found for " + VIEW_ID);
+				return;
 			}
-			String everything = sb.toString();
-			System.out.println(everything);
-		} finally {
-			io.close();
+
+			for (ReportRow row : rows) {
+				List<String> dimensions = row.getDimensions();
+				List<DateRangeValues> metrics = row.getMetrics();
+
+				for (int i = 0; i < dimensionHeaders.size() && i < dimensions.size(); i++) {
+					System.out.println(dimensionHeaders.get(i) + ": " + dimensions.get(i));
+				}
+
+				for (int j = 0; j < metrics.size(); j++) {
+					System.out.print("Date Range (" + j + "): ");
+					DateRangeValues values = metrics.get(j);
+					for (int k = 0; k < values.getValues().size() && k < metricHeaders.size(); k++) {
+						System.out.println(metricHeaders.get(k).getName() + ": " + values.getValues().get(k));
+					}
+				}
+			}
 		}
-
-		GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY,
-				new InputStreamReader(Main.class.getResourceAsStream("./client_secrets.json")));
-
-		// set up authorization code flow
-		GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(httpTransport, JSON_FACTORY,
-				clientSecrets, Collections.singleton(AnalyticsReportingScopes.ANALYTICS_READONLY))
-						.setDataStoreFactory(dataStoreFactory).build();
-		// authorize
-		return new AuthorizationCodeInstalledApp(flow, new LocalServerReceiver()).authorize("user");
 	}
-
 }
